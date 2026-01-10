574208a3bb5854497447c43428de7cbb7349508e
Teddy Li <teddyxlandlee@hotmail.com>
2026-01-10 12:15:47 +0800

    Fabric implementation

--- fabric/build.gradle.kts
+++ fabric/build.gradle.kts
@@ -3,8 +3,14 @@ plugins {
 }
 
 dependencies {
-    add("minecraft", "com.mojang:minecraft:${rootProject.ext["minecraft_version"]!!}")
-    add("mappings", loom.officialMojangMappings())
+    minecraft("com.mojang:minecraft:${rootProject.ext["minecraft_version"]!!}")
+    mappings(loom.officialMojangMappings())
+
+    modImplementation("net.fabricmc:fabric-loader:${rootProject.ext["fabric_loader_version"]!!}")
+
+    val fabricApiVersion: String = rootProject.ext["fabric_api_version"].toString()
+    modImplementation(fabricApi.module("fabric-command-api-v2", fabricApiVersion))
+    modRuntimeOnly(fabricApi.module("fabric-resource-loader-v0", fabricApiVersion))
 }
 
 tasks.shadowJar {
---
+++ fabric/src/main/java/xland/mcmod/simplelocalbook/fabric/SimpleLocalBookFabric.java
@@ -0,0 +1,25 @@
+package xland.mcmod.simplelocalbook.fabric;
+
+import com.mojang.brigadier.Command;
+import com.mojang.brigadier.CommandDispatcher;
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
+import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
+import net.minecraft.commands.CommandBuildContext;
+import xland.mcmod.simplelocalbook.SimpleLocalBookMain;
+
+public class SimpleLocalBookFabric {
+    public static void init() {
+        ClientCommandRegistrationCallback.EVENT.register(SimpleLocalBookFabric::registerCommands);
+    }
+
+    private static void registerCommands(CommandDispatcher<FabricClientCommandSource> dispatcher, CommandBuildContext registryAccess) {
+        dispatcher.register(
+                ClientCommandManager.literal("localnotebook")
+                        .executes(context -> {
+                            SimpleLocalBookMain.openBook(context.getSource().getClient());
+                            return Command.SINGLE_SUCCESS;
+                        })
+        );
+    }
+}
